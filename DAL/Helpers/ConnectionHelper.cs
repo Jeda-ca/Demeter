76788516631165b89b2f5750ed57a1a04a452274
Jeda-ca <jesuscarvajaljdc@gmail.com>
2025-05-19 21:46:19 -0500

    Modificion de gitignore agregando App.config y trabajo previo de la capa DAL

---
+++ DAL/Helpers/ConnectionHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DAL.Helpers
+{
+    public static class ConnectionHelper
+    {
+        private static string GetConnectionString()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DemeterDBConnection"]?.ConnectionString;
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                // Este es un punto crítico. En un entorno real, no deberías tener un fallback con credenciales.
+                // Deberías lanzar una excepción clara si la cadena de conexión no está configurada.
+                throw new ConfigurationErrorsException(
+                    "La cadena de conexión 'DemeterDBConnection' no se encontró o está vacía en el archivo de configuración del proyecto de inicio.");
+                
+                // Ejemplo de fallback SOLO para desarrollo local si no quieres usar App.config al principio:
+                // return "Server=localhost;Database=DEMETER_DB;Integrated Security=True;"; 
+            }
+            return connectionString;
+        }
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(GetConnectionString());
+        }
+        
+    }
+}
---
+++ DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DAL.Interfaces
+{
+    public interface IGenericRepository <T> where T : class
+    {
+        T Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+    }
+
+}
